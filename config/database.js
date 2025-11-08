ad5265687f27f66c66aac56551b4e78ed16aec5c
Vinaysoni0081 <vinaysoni0081@gmail.com>
2025-11-08 22:31:14 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2');
 const db = mysql.createConnection({
-  host: '127.0.0.1',  // use IPv4 loopback
+  host: '127.0.0.1', 
   user: 'root',
-  password: 'vinaysoni0081',       // update if your MySQL root user has a password
+  password: 'vinaysoni0081',     
   database: 'edutrack',
-  port: 3306          // explicitly specify port
+  port: 3306          
 });
 db.connect((err) => {
   if (err) throw err;
   console.log('MySQL connected');
 });
+
 module.exports = db;
